b6e9c8d0f32def8c86a9dd2b2f1b19b19633dbc9
Arda Canpolat <19eac05@gmail.com>
2026-03-18 00:39:40 +0100

    Add average calculation method

--- GradeHelper.cs
+++ GradeHelper.cs
@@ -1,3 +1,14 @@
 public static class GradeHelper
 {
+    public static double CalculateAverage(int[] values)
+    {
+        int sum = 0;
+
+        for (int i = 0; i < values.Length; i++)
+        {
+            sum += values[i];
+        }
+
+        return (double)sum / values.Length;
+    }
 }
