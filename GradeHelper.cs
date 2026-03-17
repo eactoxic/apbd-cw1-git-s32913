1a9d8dc80cbcc37a4f5c0c82780114064fc21a5f
Arda Canpolat <19eac05@gmail.com>
2026-03-18 00:42:57 +0100

    Add maximum calculation method

--- GradeHelper.cs
+++ GradeHelper.cs
@@ -11,4 +11,19 @@ public static class GradeHelper
 
         return (double)sum / values.Length;
     }
+
+    public static int CalculateMax(int[] values)
+    {
+        int max = values[0];
+
+        for (int i = 1; i < values.Length; i++)
+        {
+            if (values[i] > max)
+            {
+                max = values[i];
+            }
+        }
+
+        return max;
+    }
 }
