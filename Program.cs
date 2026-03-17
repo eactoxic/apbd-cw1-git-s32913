f5a14009ee25de3de42628bb866aad706821b3c3
Arda Canpolat <19eac05@gmail.com>
2026-03-18 00:59:45 +0100

    Update grade count message on main

--- Program.cs
+++ Program.cs
@@ -31,7 +31,7 @@ class Program
         int max = GradeHelper.CalculateMax(grades);
         int min = GradeHelper.CalculateMin(grades);
 
-        Console.WriteLine($"You entered {grades.Length} grades.");
+        Console.WriteLine($"Total grades entered: {grades.Length}");
         Console.WriteLine($"Average: {average}");
         Console.WriteLine($"Max: {max}");
         Console.WriteLine($"Min: {min}");
